b003bdaae25ef59b81a243276a4791534aca1524
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-07-14 07:43:25 +0530

    Add API For Genarate weather Information

--- script/main.js
+++ script/main.js
@@ -1,26 +1,67 @@
 
-const SearchButton=document.querySelector('.Search-btn');
-const CancelButton=document.querySelector('.cancel-button-diactive');
+const SearchButton = document.querySelector('.Search-btn');
+const CancelButton = document.querySelector('.cancel-button-diactive');
+const APIKey = "4a301f16cac37a61a82606d4eda16fd5";
 
+SearchButton.addEventListener('click', () => {
+    GetWeather();
 
-SearchButton.addEventListener('click',()=>{
-    const CityName=document.getElementById('txt-search-box').value;
-    if (!CityName) {
-            return;
-    }
-
-    const image=document.getElementById('image-weather');
-    const CountofTemp=document.getElementById('Number');
-    const city=document.getElementById('city');
-    const WindSpeed=document.getElementById('wind-speed-number');
-    const humidity=document.getElementById('humidity-percentage');
-
-    console.log(CityName);
-    console.log(image);
-    console.log(CountofTemp);
-    console.log(WindSpeed);
-    console.log(city);
-    console.log(humidity);
-    
 });
 
+function GetWeather() {
+    const CityName = document.getElementById('txt-search-box').value;
+
+
+    const image = document.getElementById('image-weather');
+    const CountofTemp = document.getElementById('Number');
+    const city = document.getElementById('city');
+    const WindSpeed = document.getElementById('wind-speed-number');
+    const humidity = document.getElementById('humidity-percentage');
+
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${CityName}&appid=${APIKey}&units=metric`)
+        .then((response) => response.json()).then(data => {
+
+            if (data.cod === "404") {
+                alert("Spelling Mistake Check Again");
+                return;
+            }
+
+            console.log(data);
+            CountofTemp.innerText = Math.round(data.main.temp);
+            city.innerText = data.name;
+            humidity.innerText = data.main.humidity;
+            WindSpeed.innerText = data.wind.speed;
+
+            document.querySelector(".container-mid").style.display = "block";
+            switch (data.weather.main) {
+                case "Clouds":
+                    image.src = "asset/cloud.png";
+                    break;
+                case "Clear":
+                    image.src = "asset/clear.png";
+                    break;
+                case "Rain":
+                    image.src = "asset/rain.png";
+                    break;
+                case "Drizzle":
+                    image.src = "asset/Drizzle.png";
+                    break;
+                case "Mist":
+                    image.src = "asset/mist.png";
+                    break;
+                case "Snow":
+                    image.src = "asset/snow.png";
+                    break;
+                default:
+
+                    break;
+            }
+        });
+
+
+    if (!CityName) {
+        alert("Please Enter City");
+        return;
+    }
+
+}
